d8031cf364bbb2ef06dfe4bfaca47889c3bff3a6
VECTOR\Ethan <ejohn121@msudenver.edu>
2017-07-09 19:38:18 -0600

    finished third assignment

--- 03 scheduling/intructions.cc
+++ 03 scheduling/intructions.cc
@@ -9,11 +9,70 @@
 #include <sys/wait.h>
 
 /*
-    Assignment 03 - CS 3600
-    Ethan Johnston
+This program does the following.
+1) Create handlers for two signals.
+2) Create an idle process which will be executed when there is nothing
+   else to do.
+3) Create a send_signals process that sends a SIGALRM every so often
+
+When run, it should produce the following output (approximately):
+
+$ ./a.out
+in CPU.cc at 247 main pid = 26428
+state:    1
+name:     IDLE
+pid:      26430
+ppid:     0
+slices:   0
+switches: 0
+started:  0
+in CPU.cc at 100 at beginning of send_signals getpid () = 26429
+in CPU.cc at 216 idle getpid () = 26430
+in CPU.cc at 222 going to sleep
+in CPU.cc at 106 sending signal = 14
+in CPU.cc at 107 to pid = 26428
+in CPU.cc at 148 stopped running->pid = 26430
+in CPU.cc at 155 continuing tocont->pid = 26430
+in CPU.cc at 106 sending signal = 14
+in CPU.cc at 107 to pid = 26428
+in CPU.cc at 148 stopped running->pid = 26430
+in CPU.cc at 155 continuing tocont->pid = 26430
+in CPU.cc at 106 sending signal = 14
+in CPU.cc at 107 to pid = 26428
+in CPU.cc at 115 at end of send_signals
+Terminated
+---------------------------------------------------------------------------
+Add the following functionality.
+1) Change the NUM_SECONDS to 20.
+
+2) Take any number of arguments for executables, and place each on new_list.
+    The executable will not require arguments themselves.
+
+3) When a SIGALRM arrives, scheduler() will be called. It calls
+    choose_process which currently always returns the idle process. Do the
+    following.
+    a) Update the PCB for the process that was interrupted including the
+        number of context switches and interrupts it had, and changing its
+        state from RUNNING to READY.
+    b) If there are any processes on the new_list, do the following.
+        i) Take the one off the new_list and put it on the processes list.
+        ii) Change its state to RUNNING, and fork() and execl() it.
+    c) Modify choose_process to round robin the processes in the processes
+        queue that are READY. If no process is READY in the queue, execute
+        the idle process.
+
+4) When a SIGCHLD arrives notifying that a child has exited, process_done() is
+    called. process_done() currently only prints out the PID and the status.
+    a) Add the printing of the information in the PCB including the number
+        of times it was interrupted, the number of times it was context
+        switched (this may be fewer than the interrupts if a process
+        becomes the only non-idle process in the ready queue), and the total
+        system time the process took.
+    b) Change the state to TERMINATED.
+    c) Start the idle process to use the rest of the time slice.
 */
 
-#define NUM_SECONDS 20
+#define NUM_SECONDS 3
 
 // make sure the asserts work
 #undef NDEBUG
@@ -21,17 +80,17 @@
 
 #define EBUG
 #ifdef EBUG
-#define dmess(a) cout << "in " << __FILE__ << \
+#   define dmess(a) cout << "in " << __FILE__ << \
     " at " << __LINE__ << " " << a << endl;
 
-#define dprint(a) cout << "in " << __FILE__ << \
+#   define dprint(a) cout << "in " << __FILE__ << \
     " at " << __LINE__ << " " << (#a) << " = " << a << endl;
 
-#define dprintt(a,b) cout << "in " << __FILE__ << \
+#   define dprintt(a,b) cout << "in " << __FILE__ << \
     " at " << __LINE__ << " " << a << " " << (#b) << " = " \
     << b << endl
 #else
-#define dprint(a)
+#   define dprint(a)
 #endif /* EBUG */
 
 using namespace std;
@@ -57,7 +116,7 @@ void (*ISV[32])(int) = {
 struct PCB
 {
     STATE state;
-    const char* name;   // name of the executable
+    const char *name;   // name of the executable
     int pid;            // process id from fork();
     int ppid;           // parent process id
     int interrupts;     // number of times interrupted
@@ -85,7 +144,7 @@ ostream& operator << (ostream &os, struct PCB *pcb)
 */
 ostream& operator << (ostream &os, list<PCB *> which)
 {
-    list<PCB*>::iterator PCB_iter;
+    list<PCB *>::iterator PCB_iter;
     for (PCB_iter = which.begin(); PCB_iter != which.end(); PCB_iter++)
     {
         os << (*PCB_iter);
@@ -93,12 +152,12 @@ ostream& operator << (ostream &os, list<PCB *> which)
     return (os);
 }
 
-PCB* running;
-PCB* idle;
+PCB *running;
+PCB *idle;
 
 // http://www.cplusplus.com/reference/list/list/
-list<PCB*> new_list;
-list<PCB*> processes;
+list<PCB *> new_list;
+list<PCB *> processes;
 
 int sys_time;
 
@@ -144,6 +203,7 @@ struct sigaction *create_handler (int signum, void (*handler)(int))
     {
         action->sa_flags = 0;
     }
+
     sigemptyset (&(action->sa_mask));
 
     assert (sigaction (signum, action, NULL) == 0);
@@ -152,27 +212,6 @@ struct sigaction *create_handler (int signum, void (*handler)(int))
 
 PCB* choose_process ()
 {
-    list<PCB*>::iterator process_iter;
-    for (process_iter = processes.begin(); process_iter != processes.end(); process_iter++)
-    {
-        PCB* pending = *process_iter;
-
-        if (pending->state == READY)
-        {
-            if (pending != running)
-            {
-                running->switches++;
-            }
-            // move chosen element to back to cycle round robin
-            processes.splice(processes.end(), processes, process_iter);
-
-            PCB* selected = processes.back();
-            running = selected;
-            return selected;
-        }
-    }
-
-    // if no process is ready, idle
     return idle;
 }
 
@@ -180,34 +219,6 @@ void scheduler (int signum)
 {
     assert (signum == SIGALRM);
     sys_time++;
-
-    running->interrupts++;
-    running->state = READY;
-
-    if (new_list.size() > 0)
-    {
-        PCB* activating = new_list.front();
-
-        activating->state = RUNNING;
-        activating->started = sys_time;
-
-        int newpid = fork();
-        assert(newpid >= 0);
-
-        if (newpid > 0) { // parent process
-            activating->pid = newpid;
-        }
-        else // newpid == 0, child process
-        {
-            activating->pid = getpid();
-
-            execl(activating->name, activating->name + 2, (char*) NULL);
-            assert(errno == 0);
-        }
-
-        processes.push_back(activating);
-        new_list.pop_front();
-    }
 
     PCB* tocont = choose_process();
 
@@ -222,8 +233,6 @@ void scheduler (int signum)
 void process_done (int signum)
 {
     assert (signum == SIGCHLD);
-
-    running->interrupts++;
 
     int status, cpid;
 
@@ -242,29 +251,7 @@ void process_done (int signum)
     }
     else
     {
-        list<PCB*>::iterator process_iter;
-        for (process_iter = processes.begin(); process_iter != processes.end(); process_iter++)
-        {
-            PCB* child = *process_iter;
-
-            cout << "comparing cpid: " << cpid << " against: " << child->pid << "\n";
-
-            if (child->pid == cpid)
-            {
-                child->state = TERMINATED;
-                dprintt ("for terminated child", child->interrupts);
-                dprintt ("for terminated child", child->switches);
-                cout << "total system time for child process " << cpid << " was: " << sys_time - child->started << "\n";
-            }
-        }
-
         dprint (WEXITSTATUS (status));
-
-        if (kill (idle->pid, SIGCONT) == -1)
-        {
-            perror ("kill");
-            return;
-        }
     }
 }
 
@@ -345,26 +332,8 @@ int main (int argc, char **argv)
     int pid = getpid();
     dprintt ("main", pid);
 
-    // modification of code by kendra lamb posted on slack
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            cout << "Adding argument " << argv[i] << " to new_list...";
-
-            // note: pid and started are initialized in choose_process
-            PCB* process = new (PCB);
-            process->state = NEW;
-            process->name = argv[i];
-            process->ppid = 0;
-            process->interrupts = 0;
-            process->switches = 0;
-
-            cout << process;
-            new_list.push_back(process);
-        }
-        //cout << new_list;
-    }
-
     sys_time = 0;
+
     boot (pid);
 
     // create a process to soak up cycles
