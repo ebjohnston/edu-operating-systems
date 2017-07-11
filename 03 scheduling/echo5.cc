5764e94474abc3b00abb10e05b57482d3e81d96f
VECTOR\Ethan <ejohn121@msudenver.edu>
2017-07-11 16:12:16 -0600

    cleaned up third assignment

---
+++ 03 scheduling/echo5.cc
@@ -0,0 +1,16 @@
+#include <iostream>   // needed for cout
+#include <stdlib.h>   // needed for exit
+#include <unistd.h>   // needed for getpid
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        cout << "Process: " << getpid() << " " << i << "\n";
+    }
+
+    exit(0);
+}
